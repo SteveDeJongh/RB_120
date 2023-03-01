79529fc5d50b6126a4f731265aba8dc63ce232a5
Steve De Jongh <de.jongh.steve@gmail.com>
2023-02-28 16:30:10 -0800

    Object Oriented Programming book pt 4

--- LS_OOP_Book.rb
+++ LS_OOP_Book.rb
@@ -449,6 +449,465 @@ end
 bob = Person.new("Steve")
 bob.name = "Bob"
 
+#### Class Inheritance
+
+class Animal
+  def speak
+    "Hello!"
+  end
+end
+
+class GoodDog < Animal # GoodDog inherits the `Animal` class. GoodDog is the subclass, `Animal` the superclass
+end
+
+class Cat < Animal
+end
+
+sparky = GoodDog.new
+paws = Cat.new
+puts sparky.speak           # => Hello!
+puts paws.speak             # => Hello!
+
+############## Exercises #############
+
+1) Create a superclass called Vehicle for your MyCar class to inherit from and move the behavior that
+ isn't specific to the MyCar class to the superclass. Create a constant in your MyCar class that stores
+  information about the vehicle that makes it different from other types of Vehicles.
+
+Then create a new class called MyTruck that inherits from your superclass that also has a constant
+ defined that separates it from the MyCar class in some way.
+
+class Vehicle
+  attr_accessor :color # Setter and getter methods
+  attr_reader :year # getter method
+  attr_reader :model # getter method
+  
+  def initialize(y, c, m)
+    @year = y
+    @color = c
+    @model = m
+    @speed = 0
+  end
+
+  def self.gas_mileage(gallons, miles) # calling self insinuates its a class method.
+    puts "#{miles/gallons} miles per gallon of gas"
+  end
+
+  def speed_up(num)
+   @speed += num
+   puts "You sped up to #{@speed} kph"
+  end
+
+  def brake(num)
+    @speed -= num
+    puts "You braked down to #{@speed} kph"
+  end
+
+  def current_speed
+    puts "You are now going #{@speed} kph"
+  end
+
+  def turn_off()
+    @speed = 0
+    puts "You turned off the car, speed is now #{@speed} kph"
+  end
+
+  def spray_paint(col)
+    self.color = col #self.color uses the setter method on :colour from `attr_accessor`. Alternatively, it could just be `@color = col`
+    puts "Your new #{color} paint job looks great!"
+  end
+end
+
+class MyCar < Vehicle # MyCar inherits from superclass `Vehicle`
+  NUMBER_OF_DOORS = 4
+
+  def to_s # Overrides "to_s" on puts... call.
+    "My car is a #{year}, #{color}, #{model}" # using attr methods to retrieve instance variables.
+  end
+end
+
+class MyTruck < Vehicle # MyTruck also inherits from superclass `Vehicle`
+  NUMBER_OF_DOORS = 2
+
+  def to_s # Overrides "to_s" on puts... call.
+    "My truck is a #{year}, #{color}, #{model}" # using attr methods to retrieve instance variables.
+  end
+end
+
+Subaru = MyCar.new(2021, "Blue", "Subaru Crosstrek")
+Toyota = MyTruck.new(2017, "Red", "Tacoma")
+
+puts Toyota
+puts Subaru
+
+2) Add a class variable to your superclass that can keep track of the number of objects created that inherit from the
+ superclass. Create a method to print out the value of this class variable as well.
+
+class Vehicle
+  attr_accessor :color # Setter and getter methods
+  attr_reader :year # getter method
+  attr_reader :model # getter method
+
+  @@number_of_vehicles = 0 # Class variable
+
+  def self.number_of_vehicles # Class method
+    puts "This program has created #{@@number_of_vehicles} vehicles."
+  end
+  
+  def initialize(y, c, m)
+    @@number_of_vehicles += 1
+    @year = y
+    @color = c
+    @model = m
+    @speed = 0
+  end
+
+  def self.gas_mileage(gallons, miles) # calling self insinuates its a class method.
+    puts "#{miles/gallons} miles per gallon of gas"
+  end
+
+  def speed_up(num)
+   @speed += num
+   puts "You sped up to #{@speed} kph"
+  end
+
+  def brake(num)
+    @speed -= num
+    puts "You braked down to #{@speed} kph"
+  end
+
+  def current_speed
+    puts "You are now going #{@speed} kph"
+  end
+
+  def turn_off()
+    @speed = 0
+    puts "You turned off the car, speed is now #{@speed} kph"
+  end
+
+  def spray_paint(col)
+    self.color = col #self.color uses the setter method on :colour from `attr_accessor`. Alternatively, it could just be `@color = col`
+    puts "Your new #{color} paint job looks great!"
+  end
+end
+
+class MyCar < Vehicle
+  NUMBER_OF_DOORS = 4
+
+  def to_s # Overrides "to_s" on puts... call.
+    "My car is a #{year}, #{color}, #{model}" # using attr methods to retrieve instance variables.
+  end
+end
+
+class MyTruck < Vehicle
+  NUMBER_OF_DOORS = 2
+
+  def to_s # Overrides "to_s" on puts... call.
+    "My truck is a #{year}, #{color}, #{model}" # using attr methods to retrieve instance variables.
+  end
+end
+
+Subaru = MyCar.new(2021, "Blue", "Subaru Crosstrek")
+Toyota = MyTruck.new(2017, "Red", "Tacoma")
+
+puts Vehicle.number_of_vehicles
+
+3) Create a module that you can mix in to ONE of your subclasses that describes a behavior unique to that subclass.
+
+class Vehicle
+  attr_accessor :color # Setter and getter methods
+  attr_reader :year # getter method
+  attr_reader :model # getter method
+
+  @@number_of_vehicles = 0 # Class variable
+
+  def self.number_of_vehicles # Class method
+    puts "This program has created #{@@number_of_vehicles} vehicles."
+  end
+  
+  def initialize(y, c, m)
+    @@number_of_vehicles += 1
+    @year = y
+    @color = c
+    @model = m
+    @speed = 0
+  end
+
+  def self.gas_mileage(gallons, miles) # calling self insinuates its a class method.
+    puts "#{miles/gallons} miles per gallon of gas"
+  end
+
+  def speed_up(num)
+   @speed += num
+   puts "You sped up to #{@speed} kph"
+  end
+
+  def brake(num)
+    @speed -= num
+    puts "You braked down to #{@speed} kph"
+  end
+
+  def current_speed
+    puts "You are now going #{@speed} kph"
+  end
+
+  def turn_off()
+    @speed = 0
+    puts "You turned off the car, speed is now #{@speed} kph"
+  end
+
+  def spray_paint(col)
+    self.color = col #self.color uses the setter method on :colour from `attr_accessor`. Alternatively, it could just be `@color = col`
+    puts "Your new #{color} paint job looks great!"
+  end
+end
+
+module Towable
+  def can_tow?(pounds)
+    pounds < 2000
+  end
+end
+
+class MyCar < Vehicle
+  NUMBER_OF_DOORS = 4
+
+  def to_s # Overrides "to_s" on puts... call.
+    "My car is a #{year}, #{color}, #{model}" # using attr methods to retrieve instance variables.
+  end
+end
+
+class MyTruck < Vehicle
+  include Towable
+  NUMBER_OF_DOORS = 2
+
+  def to_s # Overrides "to_s" on puts... call.
+    "My truck is a #{year}, #{color}, #{model}" # using attr methods to retrieve instance variables.
+  end
+end
+
+Subaru = MyCar.new(2021, "Blue", "Subaru Crosstrek")
+Toyota = MyTruck.new(2017, "Red", "Tacoma")
+
+# puts Subaru.can_tow?(300) # Undefined method for MyCar object class
+puts Toyota.can_tow?(300) # Returns true, `can_tow?` included in MyTruck class and evaluates to true.
+
+4) Print to the screen your method lookup for the classes that you have created.
+
+puts MyCar.ancestors
+puts MyTruck.ancestors
+puts Vehicle.ancestors
+
+5) Move all of the methods from the MyCar class that also pertain to the MyTruck class into the Vehicle class. Make sure that
+ all of your previous method calls are working when you are finished.
+
+class Vehicle
+  attr_accessor :color # Setter and getter methods
+  attr_reader :year # getter method
+  attr_reader :model # getter method
+
+  @@number_of_vehicles = 0 # Class variable
+
+  def self.number_of_vehicles # Class method
+    puts "This program has created #{@@number_of_vehicles} vehicles."
+  end
+  
+  def initialize(y, c, m)
+    @@number_of_vehicles += 1
+    @year = y
+    @color = c
+    @model = m
+    @speed = 0
+  end
+
+  def self.gas_mileage(gallons, miles) # calling self insinuates its a class method.
+    puts "#{miles/gallons} miles per gallon of gas"
+  end
+
+  def speed_up(num)
+   @speed += num
+   puts "You sped up to #{@speed} kph"
+  end
+
+  def brake(num)
+    @speed -= num
+    puts "You braked down to #{@speed} kph"
+  end
+
+  def current_speed
+    puts "You are now going #{@speed} kph"
+  end
+
+  def turn_off()
+    @speed = 0
+    puts "You turned off the car, speed is now #{@speed} kph"
+  end
+
+  def spray_paint(col)
+    self.color = col #self.color uses the setter method on :colour from `attr_accessor`. Alternatively, it could just be `@color = col`
+    puts "Your new #{color} paint job looks great!"
+  end
+end
+
+module Towable
+  def can_tow?(pounds)
+    pounds < 2000
+  end
+end
+
+class MyCar < Vehicle
+  NUMBER_OF_DOORS = 4
+
+  def to_s # Overrides "to_s" on puts... call.
+    "My car is a #{year}, #{color}, #{model}" # using attr methods to retrieve instance variables.
+  end
+end
+
+class MyTruck < Vehicle
+  include Towable
+  NUMBER_OF_DOORS = 2
+
+  def to_s # Overrides "to_s" on puts... call.
+    "My truck is a #{year}, #{color}, #{model}" # using attr methods to retrieve instance variables.
+  end
+end
+
+Subaru = MyCar.new(2021, "Blue", "Subaru Crosstrek")
+Toyota = MyTruck.new(2017, "Red", "Tacoma")
+
+puts Subaru.speed_up(20)
+puts Toyota.spray_paint("blue")
+puts Toyota
+puts MyCar.ancestors
+puts MyTruck.ancestors
+puts Vehicle.ancestors
+
+6) Write a method called age that calls a private method to calculate the age of the vehicle. Make sure the 
+private method is not available from outside of the class. You'll need to use Ruby's built-in Time class to help.
+
+class Vehicle
+  attr_accessor :color # Setter and getter methods
+  attr_reader :year # getter method
+  attr_reader :model # getter method
+
+  @@number_of_vehicles = 0 # Class variable
+
+  def age
+    puts "Your #{model} is #{years_old} years old."
+  end
+
+  def self.number_of_vehicles # Class method
+    puts "This program has created #{@@number_of_vehicles} vehicles."
+  end
+  
+  def initialize(y, c, m)
+    @@number_of_vehicles += 1
+    @year = y
+    @color = c
+    @model = m
+    @speed = 0
+  end
+
+  def self.gas_mileage(gallons, miles) # calling self insinuates its a class method.
+    puts "#{miles/gallons} miles per gallon of gas"
+  end
+
+  def speed_up(num)
+   @speed += num
+   puts "You sped up to #{@speed} kph"
+  end
+
+  def brake(num)
+    @speed -= num
+    puts "You braked down to #{@speed} kph"
+  end
+
+  def current_speed
+    puts "You are now going #{@speed} kph"
+  end
+
+  def turn_off()
+    @speed = 0
+    puts "You turned off the car, speed is now #{@speed} kph"
+  end
+
+  def spray_paint(col)
+    self.color = col #self.color uses the setter method on :colour from `attr_accessor`. Alternatively, it could just be `@color = col`
+    puts "Your new #{color} paint job looks great!"
+  end
+
+  private
+
+  def years_old
+    t = Time.now.year
+    years_old = t - year.to_i # If year is initiailized as a string.
+  end
+end
+
+module Towable
+  def can_tow?(pounds)
+    pounds < 2000
+  end
+end
+
+class MyCar < Vehicle
+  NUMBER_OF_DOORS = 4
+
+  def to_s # Overrides "to_s" on puts... call.
+    "My car is a #{year}, #{color}, #{model}" # using attr methods to retrieve instance variables.
+  end
+end
+
+class MyTruck < Vehicle
+  include Towable
+  NUMBER_OF_DOORS = 2
+
+  def to_s # Overrides "to_s" on puts... call.
+    "My truck is a #{year}, #{color}, #{model}" # using attr methods to retrieve instance variables.
+  end
+end
+
+Subaru = MyCar.new(2021, "Blue", "Subaru Crosstrek")
+Toyota = MyTruck.new(2017, "Red", "Tacoma")
+
+puts Subaru.age
+
+7) Create a class 'Student' with attributes name and grade. Do NOT make the grade getter public, so joe.grade will
+ raise an error. Create a better_grade_than? method, that you can call like so...
+
+class Student
+  attr_accessor :name
+
+  def initialize(n, g)
+    @name = n
+    @grade = g
+  end
+
+  def better_grade_than?(other_student)
+    grade > other_student.grade
+  end
+
+  protected
+  
+  attr_reader :grade
+
+end
+
+joe = Student.new("joe", 80)
+bob = Student.new("bob", 70)
+
+puts "Well done!" if joe.better_grade_than?(bob)
+
+8) Given the following code...
+bob = Person.new
+bob.hi
+And the corresponding error message...
+NoMethodError: private method `hi' called for #<Person:0x007ff61dbb79f0>
+from (irb):8
+from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+What is the problem and how would you go about fixing it?
+
+The `hi` method called is a private method and therefor unavailable to the object. I would fix this by moving the `hi`
+method above the `private` method call in the class.
 
 =end
-
