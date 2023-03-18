0bee4cbd628bcd56ecc4caf57fcf46594238ff4d
Steve De Jongh <de.jongh.steve@gmail.com>
2023-03-18 15:49:21 -0700

    RB 120 Lesson 5, Medium 1

--- RB-120-Practice-Problems/medium_1.rb
+++ RB-120-Practice-Problems/medium_1.rb
@@ -573,6 +573,147 @@ end
 game = GuessingGame.new(1, 100)
 game.play
 
+# 8) Highest and Lowest Ranking Cards
+
+class Card
+  include Comparable
+
+  attr_reader :rank, :suit
+  VALUES = { 'Jack' => 11, 'Queen' => 12, 'King' => 13, 'Ace' => 14 }
+  SUITS = { 'Spades' => 0.4, 'Hearts' => 0.3, 'Clubs' => 0.2, 'Diamonds' => 0.1} #using floats to avoid x of Spades being > x+1 of diamonds.
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def <=>(other)
+    score <=> other.score
+  end
+
+  def score
+    # case self.rank
+    # when "Ace"   then 14
+    # when "King"  then 13
+    # when "Queen" then 12
+    # when "Jack"  then 11
+    # else              self.rank
+    # end
+
+    #Could also use a hash and fetch with default values.
+    VALUES.fetch(rank, rank) + SUITS.fetch(suit) # FE to add score of the suit as well.
+  end
+
+  def to_s
+    "#{rank} of #{suit}"
+  end
+end
+
+# cards = [Card.new(2, 'Hearts'),
+#   Card.new(10, 'Diamonds'),
+#   Card.new('Ace', 'Clubs')]
+# puts cards
+# puts cards.min
+# puts cards.min == Card.new(2, 'Hearts')
+# puts cards.max == Card.new('Ace', 'Clubs')
+
+# cards = [Card.new(5, 'Hearts')]
+# puts cards.min == Card.new(5, 'Hearts')
+# puts cards.max == Card.new(5, 'Hearts')
+
+# cards = [Card.new(4, 'Hearts'),
+#   Card.new(4, 'Diamonds'),
+#   Card.new(10, 'Clubs')]
+# puts cards.min.rank == 4
+# puts cards.max == Card.new(10, 'Clubs')
+
+# cards = [Card.new(7, 'Diamonds'),
+#   Card.new('Jack', 'Diamonds'),
+#   Card.new('Jack', 'Spades')]
+# puts cards.min == Card.new(7, 'Diamonds')
+# puts cards.max.rank == 'Jack'
+
+# cards = [Card.new(8, 'Diamonds'),
+#   Card.new(8, 'Clubs'),
+#   Card.new(8, 'Spades')]
+# puts cards.min.rank == 8
+# puts cards.max.rank == 8
+
+cards = [Card.new(5, 'Diamonds'),
+  Card.new(4, 'Diamonds'),
+  Card.new(4, 'Spades')]
+
+p cards.min
+p cards.max
+
+# 9) Deck of Cards
+
+class Card
+  include Comparable
+
+  attr_reader :rank, :suit
+  VALUES = { 'Jack' => 11, 'Queen' => 12, 'King' => 13, 'Ace' => 14 }
+  # SUITS = { 'Spades' => 0.4, 'Hearts' => 0.3, 'Clubs' => 0.2, 'Diamonds' => 0.1} #using floats to avoid x of Spades being > x+1 of diamonds.
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def <=>(other)
+    score <=> other.score
+  end
+
+  def score
+    # case self.rank
+    # when "Ace"   then 14
+    # when "King"  then 13
+    # when "Queen" then 12
+    # when "Jack"  then 11
+    # else              self.rank
+    # end
+
+    #Could also use a hash and fetch with default values.
+    VALUES.fetch(rank, rank) #+ SUITS.fetch(suit) # FE to add score of the suit as well.
+  end
+
+  def to_s
+    "#{rank} of #{suit}"
+  end
+end
+
+class Deck
+  RANKS = ((2..10).to_a + %w(Jack Queen King Ace)).freeze
+  SUITS = %w(Hearts Clubs Diamonds Spades).freeze
+
+  def initialize
+    reset
+  end
+
+  def draw
+    reset if @deck.empty?
+    @deck.pop
+  end
+
+  private
+
+  def reset
+    @deck = RANKS.product(SUITS).map {|x, y| Card.new(x, y) }.shuffle
+  end
+end
+
+deck = Deck.new
+drawn = []
+52.times { drawn << deck.draw }
+# p drawn
+p drawn.count { |card| card.rank == 5 } == 4
+p drawn.count { |card| card.suit == 'Hearts' } == 13
+
+drawn2 = []
+52.times { drawn2 << deck.draw }
+p drawn != drawn2 # Almost always.
+
+
 =end
 
-# 8) 
+# 10) Poker!
