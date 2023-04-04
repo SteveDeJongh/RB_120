cb1974966aef85b93e8f390e3369d4cf5196f27f
Steve De Jongh <de.jongh.steve@gmail.com>
2023-04-04 16:25:24 -0700

    RB 129 Interview prep

--- RB-120-Practice-Problems/sample_problems.rb
+++ RB-120-Practice-Problems/sample_problems.rb
@@ -320,5 +320,120 @@ lib.books.each { |book| puts book }
 lib.checkout_book('The Odyssey', 'Homer')
   # => The library does not have that book
 
-=end
+# Constants
 
+LOCATION = self
+
+class Parent
+  # LOCATION = self
+end
+
+module A
+  module B
+    # LOCATION = self
+    module C
+      class Child < Parent
+        # LOCATION = self
+        def where_is_the_constant
+          LOCATION
+        end
+      end
+    end
+  end
+end
+
+instance = A::B::C::Child.new
+puts instance.where_is_the_constant
+
+# What does the last line of code output? #=> A::B::C::Child
+# Comment out LOCATION in Child, what is output now? #=> A::B
+# Comment out LOCATION in Module B, what is output now? #=> Parent
+# Comment out LOCATION in Parent, what is output now? #=> main
+
+# Implement the given classes so that we get the expected results
+
+class ClassLevel
+  attr_accessor :level, :members
+
+  def initialize(level)
+    @level = level
+    @members = []
+  end
+
+  def <<(student)
+    if members.include?(student)
+      puts "That student is already added."
+    else
+      members << student
+    end
+  end
+
+  def valedictorian
+    top = members.max_by {|student| student.gpa}
+    puts "#{top.name} has the highest GPA of #{top.gpa}"
+  end
+end
+
+class Student
+  attr_accessor :name, :id, :gpa
+  
+  def initialize(name, id, gpa)
+    @name = name
+    @id = id
+    @gpa = gpa
+  end
+
+  def ==(other)
+    name == other.name && id == other.id && gpa == other.gpa
+  end
+
+  def >(other)
+    gpa > other.gpa
+  end
+
+  def to_s
+    <<~HEREDOC
+    ===========
+    Name: #{name}
+    Id: #{id}
+    GPA: #{gpa}
+    ==========
+    HEREDOC
+  end
+end
+
+juniors = ClassLevel.new('Juniors')
+
+anna_a = Student.new('Anna', '123-11-123', 3.85)
+bob = Student.new('Bob', '555-44-555', 3.23)
+chris = Student.new('Chris', '321-99-321', 2.98)
+david = Student.new('David', '987-00-987', 3.12)
+anna_b = Student.new('Anna', '543-33-543', 3.76)
+
+juniors << anna_a
+juniors << bob
+juniors << chris
+juniors << david
+juniors << anna_b
+
+juniors << anna_a
+  # => "That student is already added"
+
+puts juniors.members
+  # => ===========
+  # => Name: Anna
+  # => Id: XXX-XX-123
+  # => GPA: 3.85
+  # => ==========
+  # => ...etc (for each student)
+
+p anna_a == anna_b 
+  # => false
+
+p david > chris
+  # => true
+
+juniors.valedictorian
+  # => "Anna has the highest GPA of 3.85"
+
+  =end
